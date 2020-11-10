e4246a5112317b4719fc2b5a53416fa728d5817c
Alexandre <alexandresmoraes@me.com>
2020-11-10 15:01:22 -0300

    Adicionado Swagger.

---
+++ src/Todo.Infra.CrossCutting.Swagger/Config.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Todo.Infra.CrossCutting.Swagger
+{
+  public static class Config
+  {
+    private const string API_NAME = "Todo Api";
+    private const string API_VERSION = "1.0.0.0";
+    private const string API_VERSION_SHORT = "1";
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = API_NAME,
+          Version = API_VERSION,
+          Description = "Todo skills",
+        });
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+          Description = "Cabeçalho de autorização JWT usando o esquema Bearer.\r\n\r\n" +
+          "Digite 'Bearer' [espaço] e, em seguida, seu Bearer token na entrada de texto abaixo.\r\n\r\n" +
+          "Exemplo: \"Bearer {User_Token}\"",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+          {
+            new OpenApiSecurityScheme
+            {
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header
+            },
+            new List<string>()
+          }
+        });
+
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, "Todo.Presentation.WebApi.xml");
+        c.IncludeXmlComments(xmlPath);
+      });
+
+      return services;
+    }
+
+    public static IApplicationBuilder UseSwagger(this IApplicationBuilder app)
+    {
+      app.UseSwagger(c =>
+      {
+        c.RouteTemplate = "dev/{documentName}/docs.json";
+      });
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/dev/v1/docs.json", $"{API_NAME} V{API_VERSION_SHORT}");
+        c.DocumentTitle = API_NAME;        
+        c.RoutePrefix = "dev";
+        c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.List);
+      });
+
+      return app;
+    }
+  }
+}
--- src/Todo.Presentation.WebApi/Startup.cs
+++ src/Todo.Presentation.WebApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Todo.Infra.CrossCutting.Swagger;
 
 namespace Todo.Presentation.WebApi
 {
@@ -22,19 +16,21 @@ namespace Todo.Presentation.WebApi
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+
+      services.AddSwagger();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
+
+      app.UseSwagger();
 
       app.UseHttpsRedirection();
 
